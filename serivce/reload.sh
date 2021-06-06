63fd6728e8bea52de1d1afbcac4c580110d6a686
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2021-06-06 20:45:21 +0200

    stop command service

--- serivce/reload.sh
+++ serivce/reload.sh
@@ -2,7 +2,7 @@
 
 cd ..
 go build -o bot .
-sudo service whatsapp-signal-bridge start
+sudo service whatsapp-signal-bridge stop
 sudo systemctl disable whatsapp-signal-bridge.service
 sudo systemctl enable whatsapp-signal-bridge.service
 sudo service whatsapp-signal-bridge start
